ca0c6494aff1f4b985caced0e551b8a25983f968
Eduardo Sena <dudu94bhz@gmail.com>
2026-03-14 13:09:21 -0300

    Ajuste de Endpoints

--- DTOs/Pessoa/UpdatePessoaDto.cs
+++ DTOs/Pessoa/UpdatePessoaDto.cs
@@ -4,11 +4,7 @@ namespace ControleFinanceiro.DTOs.Pessoa;
 
 public class UpdatePessoaDto
 {
-    [Required(ErrorMessage = "O nome é obrigatório")]
-    [StringLength(200, ErrorMessage = "O nome deve ter no máximo 200 caracteres")]
     public string Nome { get; set; }
 
-    [Required(ErrorMessage = "A idade é obrigatória")]
-    [Range(0, 100, ErrorMessage = "A idade deve estar entre 0 e 100 anos")]
     public int Idade { get; set; }
 }
